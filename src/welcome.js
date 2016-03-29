f3b2c80ab3a742b29d29e2aa20ed6d6bee23ac28
Swagstopher <crystalcleare@gmail.com>
2016-03-29 04:46:57 -0700

    Styling

--- src/welcome.js
+++ src/welcome.js
@@ -3,4 +3,5 @@ export class App {
   social = 'Social Media';
   facebook = 'Facebook';
   myname = 'Chris Navy';
+  lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus placerat euismod suscipit. Nam et fringilla risus. Vestibulum auctor elit in elit pellentesque, vel interdum eros pulvinar. Vestibulum eleifend odio neque, eu porttitor ante euismod vitae. Maecenas non hendrerit arcu. Nunc porttitor orci tortor, vel mollis felis eleifend blandit. Aenean interdum feugiat elit eu laoreet. Pellentesque condimentum eu nisl nec faucibus. Nullam a mollis dolor.';
 }
